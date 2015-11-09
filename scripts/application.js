2a4cfde3aa21aee08bb68067bdb15bcfa1b7da69
Deryx E. Scott <deryxescott@gmail.com>
2015-11-09 01:02:28 -0500

    African American Historical Society: Version 1

--- scripts/application.js
+++ scripts/application.js
@@ -5,6 +5,17 @@
 $(function() {
     $( '.links li a').on( 'click', function() {
         var link = $( this ).attr( 'href');
+        $( 'a[class=active-link]' ).removeClass( 'active-link' );
+        $( this ).addClass( 'active-link' );
+        $( '.info div[id]:visible' ).hide();
+        $( link ).fadeIn(1500);
+    })
+
+    $( '.links li li a').on( 'click', function() {
+        var link = $( this ).attr( 'href');
+        console.log(link);
+        $( 'a[class=active-link]' ).removeClass( 'active-link' );
+        $( this ).addClass( 'active-link' );
         $( '.info div[id]:visible' ).hide();
         $( link ).fadeIn(1500);
     })
