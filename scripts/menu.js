fb3694cc51e9ff8c07181bde90f6c3f924109d16
Deryx <deryxescott@gmail.com>
2015-11-18 17:23:19 -0500

    African American Historical Society: Version 1

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,21 +3,7 @@
  */
 
 $(function() {
-    $( 'nav ul li a' ).on( 'mouseover', function() {
-        var $this = $(this);
-        if ( $( $this).closest( 'ul' ) ) {
-            $($this).css('background', '#DCBDA9');
-            $($this).next().show();
-        }
-    });
-
-    $( 'nav ul li a' ).on( 'mouseout', function() {
-        var $this = $(this);
-        if ( $( $this ).has( 'ul' ) ) {
-            setTimeout(function () {
-                $($this).css('background', '#000');
-                $($this).next().hide();
-            }, 3000);
-        }
+    $( '#menu').click(function() {
+        $('nav ul').toggle(1000);
     });
 });
